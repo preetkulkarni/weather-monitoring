00cc98bc658b8442b873ec1afa98ed1f7099e12f
preetkulkarni <pkz.exe@gmail.com>
2026-03-31 16:53:50 +0530

    minor fix

--- app/database.py
+++ app/database.py
@@ -18,7 +18,7 @@ engine = create_engine(
 )
 
 @event.listens_for(Engine, "connect")
-def set_sqlite_pragma(dbapi_connection, connection_record):
+def set_sqlite_pragma(dbapi_connection):
     cursor = dbapi_connection.cursor()
     cursor.execute("PRAGMA foreign_keys=ON")       
     cursor.execute("PRAGMA journal_mode=WAL")    
